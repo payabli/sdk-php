6970fedfc44d11aab99ed614b07a9aedd55444d0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-11 18:02:34 +0000

    Release 0.0.373

--- src/PayabliClient.php
+++ src/PayabliClient.php
@@ -202,8 +202,8 @@ class PayabliClient
         $defaultHeaders = [
             'X-Fern-Language' => 'PHP',
             'X-Fern-SDK-Name' => 'Payabli',
-            'X-Fern-SDK-Version' => '0.0.371',
-            'User-Agent' => 'payabli/payabli/0.0.371',
+            'X-Fern-SDK-Version' => '0.0.373',
+            'User-Agent' => 'payabli/payabli/0.0.373',
         ];
         if ($apiKey != null) {
             $defaultHeaders['requestToken'] = $apiKey;
